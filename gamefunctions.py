89096d500a5fd5bed8034ba0c961df6365b1493e
Joseph Rosteck <joseph.rosteck@umontana.edu>
2024-10-15 09:23:41 -0600

    combined adventurefunctions and gamefunctions.

--- gamefunctions.py
+++ gamefunctions.py
@@ -23,6 +23,9 @@ def print_shop_menu(item1name, item1price, item2name, item2price):
     print(bottom_border)
 
 #####################################
+print()
+print()
+#####################################
 
 print("print_welcome tests with names Bob, Ashley, and Bartholomew")
 print_welcome('Bob')
@@ -38,6 +41,86 @@ print()
 print("print_shop_menu tests with Donkey, 109.99, Orange, 6")
 print_shop_menu("Donkey",109.99,"Orange",6)
 
+####################################
+
+import random
+
+####################################
+
+def purchase_item(itemPrice, startingMoney, quantityToPurchase=1):
+    while True: #loop so if quantityToPurchase is too high 
+        if quantityToPurchase <= startingMoney // itemPrice:
+            num_purchases = quantityToPurchase
+            break
+        else:
+            quantityToPurchase = quantityToPurchase - 1
+            continue
+    
+    leftover_money = startingMoney - (itemPrice * num_purchases)
+    return num_purchases, leftover_money
+    
+###################################
+
+def new_random_monster():
+    names = ['Goblin', 'Troll', 'Dragon']
+    
+    descriptions = {
+    "Goblin": "This is a lone goblin. When it notices you, it rushes at you quickly with a sharp dagger drawn.",
+    "Troll": "A massive troll lumbers into your path, when it sees you it lets out a deafening roar.",
+    "Dragon": "You spot the majestic dragon curled on top of its horde. When you cross the threshold of the cave, its eyes snap open.",
+    }
+    
+    health_ranges = {
+    "Goblin": (3, 10),
+    "Troll": (15, 50),
+    "Dragon": (100, 300),
+    }
+    power_ranges = {
+    "Goblin": (1, 5),
+    "Troll": (10, 25),
+    "Dragon": (50, 100),
+    }
+    money_range = {
+    "Goblin": (5, 10),
+    "Troll": (25, 50),
+    "Dragon": (100, 500),
+    }
+
+    name = random.choice(names)
+    description = descriptions[name]
+    health = random.randint(*health_ranges[name])
+    power = random.randint(*power_ranges[name])
+    money = random.randint(*money_range[name])
+    
+                  
+    return {
+        "name": name,
+        "description": description,
+        "health": health,
+        "power": power,
+        "money": money,
+        }
+        
+####################################
+print()
+print()
+####################################
+
+print("purchase_item base test")
+print("item price = 1.23, starting money = 10, quantity purchased = 3")
+print(purchase_item(1.23, 10, 3),"\n")
+###
+print("purchase_item default quantity test")
+print("item price = 3, starting money = 10")
+print(purchase_item(3,10),"\n")
+###
+print("purchase_item too many items test")
+print("item price = 3, starting money = 10 quantity purchased = 29")
+print(purchase_item(3, 10, 29),"\n\n")
+###
+print(new_random_monster(),"\n")
+print(new_random_monster(),"\n")
+print(new_random_monster(),"\n")
 
     
 
